83d4c0d730900d4a6258cb2dda21798ff027f046
amanda16229 <amanda.olyer@gmail.com>
2025-12-17 20:32:28 -0500

    initial test with React

--- osec-website/src/App.js
+++ osec-website/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section id="Content">
+        <h1>Develop Your Skills With These Resources</h1>
+        <img src="/images/OSECLogoFullNoBG1.png" alt="" className="osec-logo" />
+        <h2>Learning Platforms</h2>
+        <section id="resources-list">
+          <div className="resource-item">
+            <a href="https://tryhackme.com/" target="_blank" rel="noreferrer" className="resource-button">TryHackMe</a>
+            <span className="resource-description">Hands-on cybersecurity training</span>
+          </div>
+          <div className="resource-item">
+            <a href="https://www.hackthebox.com/" target="_blank" rel="noreferrer" className="resource-button">Hack The Box</a>
+            <span className="resource-description">Penetration testing</span>
+          </div>
+          <div className="resource-item">
+            <a href="https://overthewire.org/wargames/bandit/" target="_blank" rel="noreferrer" className="resource-button">Over the wire</a>
+            <span className="resource-description">Gain experience with Linux</span>
+          </div>
+        </section>
+      </section>
+    </main>
   );
 }
 
